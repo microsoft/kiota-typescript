19fc7f62b54384e4586785663abde50b68bd8de3
Nikitha Chettiar <nikithauc@gmail.com>
2022-03-31 14:58:06 -0700

    setting process env

--- packages/test/tests/getTest.ts
+++ packages/test/tests/getTest.ts
@@ -1,11 +1,12 @@
 import { apiClient } from "./testClient";
 
+import { assert } from "chai";
 
 describe("TestGet", () => {
 
 
-it("should return a test", async () => {
-    const messages = apiClient.usersById("").messages().get();
-    assert.isDefined(messages.value);
-});
+    it("should return a test", async () => {
+        const messages = await apiClient.usersById("813956a3-4a30-4596-914f-bfd86a657a09").messages.get();
+        assert.isDefined(messages?.value);
+    });
 });
--- packages/test/tests/testClient.ts
+++ packages/test/tests/testClient.ts
@@ -3,11 +3,10 @@ import {FetchRequestAdapter} from "@microsoft/kiota-http-fetchlibrary";
 import {AzureIdentityAuthenticationProvider} from "@microsoft/kiota-authentication-azure";
 import {ClientSecretCredential} from "@azure/identity";
 
-const tokenCredential = new ClientSecretCredential(tenantId,clientid,clientsecret);
+
+const tokenCredential = new ClientSecretCredential(process.env.TENANT_ID, process.env.CLIENT_ID, process.env.CLIENT_SECRET );
 
 const authProvider = new AzureIdentityAuthenticationProvider(tokenCredential);
 const fetchRequestAdapter = new FetchRequestAdapter(authProvider)
 export const apiClient = new ApiClient(fetchRequestAdapter);
-
-
 
