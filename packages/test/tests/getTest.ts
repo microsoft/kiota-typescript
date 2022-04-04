c77f3be7ea0f33ba14c6fbd861db0058922fcb49
Nikitha Chettiar <nikithauc@gmail.com>
2022-04-03 21:05:54 -0700

    post test and es build

--- packages/test/tests/getTest.ts
+++ packages/test/tests/getTest.ts
@@ -4,7 +4,6 @@ import { assert } from "chai";
 
 describe("TestGet", () => {
 
-
     it("should return a test", async () => {
         const messages = await apiClient.usersById("813956a3-4a30-4596-914f-bfd86a657a09").messages.get();
         assert.isDefined(messages?.value);
---
+++ packages/test/tests/postTest.ts
@@ -0,0 +1,15 @@
+import { apiClient } from "./testClient";
+
+import { assert } from "chai";
+import { Message } from "../generatedCode/models/microsoft/graph";
+
+describe("TestPost", () => {
+
+    it("should return a test", async () => {
+        const message:Message =  new Message();
+        message.subject = "Test Subject";
+        const postmessageResult = await apiClient.usersById("813956a3-4a30-4596-914f-bfd86a657a09").messages.post(message);
+        assert.isDefined(postmessageResult?.id);
+        assert.equal(postmessageResult?.subject, message.subject);
+    }).timeout(10000);
+});
