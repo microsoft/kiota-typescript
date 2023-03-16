366c2eb7650ad603dad961535e6f4ed975f58898
Vincent Biret <vibiret@microsoft.com>
2023-03-16 15:49:05 -0400

    - adds a base request builder
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ packages/abstractions/src/baseRequestBuilder.ts
@@ -0,0 +1,26 @@
+import { getPathParameters } from "../dist/cjs/src";
+import { RequestAdapter } from "./requestAdapter";
+
+export abstract class BaseRequestBuilder {
+  /** Path parameters for the request */
+  protected pathParameters: Record<string, unknown>;
+  /** The request adapter to use to execute the requests. */
+  protected requestAdapter: RequestAdapter;
+  /** Url template to use to build the URL for the current request builder */
+  protected urlTemplate: string;
+  protected constructor(
+    pathParameters: Record<string, unknown> | string | undefined,
+    requestAdapter: RequestAdapter,
+    urlTemplate: string
+  ) {
+    if (!pathParameters) throw new Error("pathParameters cannot be undefined");
+    if (!requestAdapter) throw new Error("requestAdapter cannot be undefined");
+    if (urlTemplate === undefined) {
+      // empty string is ok
+      throw new Error("urlTemplate cannot be undefined");
+    }
+    this.pathParameters = getPathParameters(pathParameters);
+    this.requestAdapter = requestAdapter;
+    this.urlTemplate = urlTemplate;
+  }
+}
