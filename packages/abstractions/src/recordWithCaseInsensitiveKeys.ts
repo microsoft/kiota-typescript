98344d9476a2c8eea00d5fbf40e168043870b05a
Vincent Biret <vibiret@microsoft.com>
2023-07-24 10:10:59 -0400

    - conserves casing for entries
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- packages/abstractions/src/recordWithCaseInsensitiveKeys.ts
+++ packages/abstractions/src/recordWithCaseInsensitiveKeys.ts
@@ -3,62 +3,76 @@
  * @param canon A function that takes a property name and returns its canonical form.
  * @returns A new object that can be used as a dictionary with case-insensitive keys.
  */
-function dictionaryWithCanonicalKeys<V>(canon: (prop: keyof any) => keyof any) {
+function dictionaryWithCanonicalKeys<V>(canon: (prop: keyof any) => string) {
+  const keysNormalizationMap = new Map<string, string>();
   return new Proxy<{ [k: string]: V }>(
     {},
     {
       /**
-      * Intercept the get operation on the dictionary object and forward it to the target object using Reflect.get.
-      */
+       * Intercept the get operation on the dictionary object and forward it to the target object using Reflect.get.
+       */
       get(target, prop) {
-        return Reflect.get(target, canon(prop));
+        const normalKey = canon(prop);
+        return Reflect.get(target, normalKey);
       },
       /**
-      * Intercept the set operation on the dictionary object and forward it to the target object using Reflect.set.
-      */
+       * Intercept the set operation on the dictionary object and forward it to the target object using Reflect.set.
+       */
       set(target, prop, value) {
-        return Reflect.set(target, canon(prop), value);
+        const nonNormalKey = prop.toString();
+        const normalKey = canon(prop);
+        keysNormalizationMap.set(normalKey, nonNormalKey);
+        return Reflect.set(target, normalKey, value);
       },
       /**
-      * Intercept the has operation on the dictionary object and forward it to the target object using Reflect.has.
-      */
-      has(target, prop) {
-        return Reflect.has(target, canon(prop));
+       * Intercept the has operation on the dictionary object and forward it to the target object using Reflect.has.
+       */
+      has(_, prop) {
+        const normalKey = canon(prop);
+        return keysNormalizationMap.has(normalKey);
       },
       /**
-      * Intercept the defineProperty operation on the dictionary object and forward it to the target object using Reflect.defineProperty.
-      */
+       * Intercept the defineProperty operation on the dictionary object and forward it to the target object using Reflect.defineProperty.
+       */
       defineProperty(target, prop, attribs) {
-        return Reflect.defineProperty(target, canon(prop), attribs);
+        const nonNormalKey = prop.toString();
+        const normalKey = canon(prop);
+        keysNormalizationMap.set(normalKey, nonNormalKey);
+        return Reflect.defineProperty(target, normalKey, attribs);
       },
       /**
-      * Intercept the deleteProperty operation on the dictionary object and forward it to the target object using Reflect.deleteProperty.
-      */
+       * Intercept the deleteProperty operation on the dictionary object and forward it to the target object using Reflect.deleteProperty.
+       */
       deleteProperty(target, prop) {
-        return Reflect.deleteProperty(target, canon(prop));
+        const normalKey = canon(prop);
+        keysNormalizationMap.delete(normalKey);
+        return Reflect.deleteProperty(target, normalKey);
       },
       /**
-      * Intercept the getOwnPropertyDescriptor operation on the dictionary object and forward it to the target object using Reflect.getOwnPropertyDescriptor.
-      */
+       * Intercept the getOwnPropertyDescriptor operation on the dictionary object and forward it to the target object using Reflect.getOwnPropertyDescriptor.
+       */
       getOwnPropertyDescriptor(target, prop) {
         return Reflect.getOwnPropertyDescriptor(target, canon(prop));
-      }
-    }
+      },
+      ownKeys() {
+        return [...keysNormalizationMap.values()];
+      },
+    },
   );
 }
 
 /**
-* A wrapper for type Record that creates a Record with case insensitive keys
-* @returns A new object that can be used as a dictionary with case-insensitive keys.
-*/
+ * A wrapper for type Record that creates a Record with case insensitive keys
+ * @returns A new object that can be used as a dictionary with case-insensitive keys.
+ */
 export function createRecordWithCaseInsensitiveKeys<T>(): Record<string, T> {
   /**
-  * A function that takes a property name and returns its canonical form.
-  * @param prop The property name to be canonicalized.
-  * @returns The canonical form of the property name.
-  */
-  const record: Record<string, T> = dictionaryWithCanonicalKeys<T>(
-    p => (typeof p === "string" ? p.toLowerCase() : p)
+   * A function that takes a property name and returns its canonical form.
+   * @param prop The property name to be canonicalized.
+   * @returns The canonical form of the property name.
+   */
+  const record: Record<string, T> = dictionaryWithCanonicalKeys<T>((p) =>
+    typeof p === "string" ? p.toLowerCase() : p.toString().toLowerCase(),
   );
   return record;
 }
