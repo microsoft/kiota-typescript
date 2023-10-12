f5c16e14268ff5172cc7310c37038c702b9aa4bc
koros <korosmatick@gmail.com>
2023-10-12 16:24:13 +0300

    add unit test for backing store utility function

--- packages/abstractions/test/common/store/backingStoreUtilsTest.ts
+++ packages/abstractions/test/common/store/backingStoreUtilsTest.ts
@@ -1,41 +1,15 @@
-import { Parsable, ParsableFactory, ParseNode } from "../../../src/serialization";
+import { assert } from "chai";
 import { isBackingStoreEnabled } from "../../../src/store/backingStoreUtils";
+import { TestBackedModel, createTestBackedModelFromDiscriminatorValue, createTestParserFromDiscriminatorValue } from "./testEntity";
 
-interface Person {
-  name: string;
-  age: number;
-}
+it("Test backing store should be enabled if the parsableFactory has backingStore property", async () => {
+    const testBackedModel = {} as TestBackedModel;
+    const backingStoreEnabled = isBackingStoreEnabled(testBackedModel, createTestBackedModelFromDiscriminatorValue);
+    assert.isTrue(backingStoreEnabled);
+});
 
-// const personFactory: ParsableFactory<Person> = (node: ParseNode) => (obj: Person) => ({
-//   name: node.getString(obj.name),
-//   age: node.getNumber(obj.age),
-// });
-
-// describe('isBackingStoreEnabled', () => {
-//   it('should return true if the object has a backingStore property', () => {
-//     const person: Person = { name: 'John', age: 30 };
-//     const result = isBackingStoreEnabled(person, personFactory);
-//     expect(result).toBe(true);
-//   });
-
-//   it('should return false if the object does not have a backingStore property', () => {
-//     const person: Person = { name: 'John', age: 30 };
-//     delete person[Symbol.for('backingStore')];
-//     const result = isBackingStoreEnabled(person, personFactory);
-//     expect(result).toBe(false);
-//   });
-
-//   it('should return false if the object is not a Parsable', () => {
-//     const obj = { foo: 'bar' };
-//     const result = isBackingStoreEnabled(obj as Parsable, personFactory);
-//     expect(result).toBe(false);
-//   });
-
-//   it('should return false if the object is null or undefined', () => {
-//     const result1 = isBackingStoreEnabled(null, personFactory);
-//     expect(result1).toBe(false);
-
-//     const result2 = isBackingStoreEnabled(undefined, personFactory);
-//     expect(result2).toBe(false);
-//   });
-// });
+it("Test backing store should not be enabled if the parsableFactory lacks backingStore property", async () => {
+    const testBackedModel = {} as TestBackedModel;
+    const backingStoreEnabled = isBackingStoreEnabled(testBackedModel, createTestParserFromDiscriminatorValue);
+    assert.isFalse(backingStoreEnabled);
+});
--- packages/abstractions/test/common/store/testEntity.ts
+++ packages/abstractions/test/common/store/testEntity.ts
@@ -1,14 +1,9 @@
-import { BackedModel, BackingStore, Parsable, ParseNode, SerializationWriter } from "../../../src";
+import { BackedModel, BackingStore, DateOnly, Duration, Parsable, ParsableFactory, ParseNode, SerializationWriter, TimeOnly, createBackedModelProxyHandler, isBackingStoreEnabled, parseGuidString } from "../../../src";
 
 const fakeBackingStore: BackingStore = {} as BackingStore;
 
 export interface TestParser {
-  testCollection?: string[] | undefined;
   testString?: string | undefined;
-  testComplexString?: string | undefined;
-  testObject?: Record<string, unknown> | undefined;
-  additionalData?: Record<string, unknown>;
-  testDate?: Date | undefined;
   foos?: FooResponse[] | undefined;
 }
 export interface TestBackedModel extends TestParser, BackedModel {
@@ -55,18 +50,6 @@ export function deserializeTestParser(
   testParser: TestParser | undefined = {}
 ): Record<string, (node: ParseNode) => void> {
   return {
-    testCollection: (n) => {
-      testParser.testCollection = n.getCollectionOfPrimitiveValues();
-    },
-    testString: (n) => {
-      testParser.testString = n.getStringValue();
-    },
-    textComplexString: (n) => {
-      testParser.testComplexString = n.getStringValue();
-    },
-    testDate: (n) => {
-      testParser.testDate = n.getDateValue()
-    },
     foos: (n) => {
       testParser.foos = n.getCollectionOfObjectValues(createFooParserFromDiscriminatorValue);
     }
@@ -79,18 +62,6 @@ export function deserializeTestBackedModel(
   return {
     backingStore: (n) => {
       testParser.backingStore = fakeBackingStore;
-    },
-    testCollection: (n) => {
-      testParser.testCollection = n.getCollectionOfPrimitiveValues();
-    },
-    testString: (n) => {
-      testParser.testString = n.getStringValue();
-    },
-    textComplexString: (n) => {
-      testParser.testComplexString = n.getStringValue();
-    },
-    testDate: (n) => {
-      testParser.testDate = n.getDateValue()
     },
     foos: (n) => {
       testParser.foos = n.getCollectionOfObjectValues(createFooParserFromDiscriminatorValue);
@@ -125,26 +96,4 @@ export function deserializeBarParser(
       barResponse.propC = n.getDateValue();
     }
   };
-}
-
-export function serializeTestObject(
-  writer: SerializationWriter,
-  entity: { additionalData?: Record<string, unknown> } | undefined = {}
-): void {
-  writer.writeAdditionalData(entity.additionalData);
-}
-export function serializeTestParser(
-  writer: SerializationWriter,
-  entity: TestParser | undefined = {}
-): void {
-  writer.writeCollectionOfPrimitiveValues(
-    "testCollection",
-    entity.testCollection
-  );
-  writer.writeStringValue("testString", entity.testString);
-  writer.writeStringValue("testComplexString", entity.testComplexString);
-
-  writer.writeDateValue("testDate", entity.testDate);
-  writer.writeObjectValue("testObject", entity.testObject, serializeTestObject);
-  writer.writeAdditionalData(entity.additionalData);
 }
