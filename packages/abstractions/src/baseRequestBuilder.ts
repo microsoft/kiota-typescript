434e4ba31d696f5f0988691ecb65c787457f8e39
Vincent Biret <vibiret@microsoft.com>
2023-12-01 14:12:25 -0500

    - adds the withUrl method to the base request builder

--- packages/abstractions/src/baseRequestBuilder.ts
+++ packages/abstractions/src/baseRequestBuilder.ts
@@ -1,25 +1,43 @@
 import { getPathParameters } from "./getPathParameters";
 import type { RequestAdapter } from "./requestAdapter";
 
-export abstract class BaseRequestBuilder {
+export abstract class BaseRequestBuilder<T> {
   /** Path parameters for the request */
   protected pathParameters: Record<string, unknown>;
   /** The request adapter to use to execute the requests. */
   protected requestAdapter: RequestAdapter;
   /** Url template to use to build the URL for the current request builder */
   protected urlTemplate: string;
+  private withUrlFactory: WithUrlFactory<T>;
   protected constructor(
     pathParameters: Record<string, unknown> | string | undefined,
     requestAdapter: RequestAdapter,
-    urlTemplate: string
+    urlTemplate: string,
+    factory: WithUrlFactory<T>,
   ) {
     if (!requestAdapter) throw new Error("requestAdapter cannot be undefined");
     if (urlTemplate === undefined) {
       // empty string is ok
       throw new Error("urlTemplate cannot be undefined");
     }
+    if (!factory) throw new Error("factory cannot be undefined");
     this.pathParameters = getPathParameters(pathParameters);
     this.requestAdapter = requestAdapter;
     this.urlTemplate = urlTemplate;
+    this.withUrlFactory = factory;
+  }
+  /**
+   * Returns a request builder with the provided arbitrary URL. Using this method means any other path or query parameters are ignored.
+   * @param rawUrl The raw URL to use for the request builder.
+   * @returns a MessageItemRequestBuilder
+   */
+  public withUrl(rawUrl: string): T {
+    if (!rawUrl) throw new Error("rawUrl cannot be undefined");
+    return this.withUrlFactory(rawUrl, this.requestAdapter);
   }
 }
+
+type WithUrlFactory<T> = (
+  pathParameters: Record<string, unknown> | string | undefined,
+  requestAdapter: RequestAdapter,
+) => T;
