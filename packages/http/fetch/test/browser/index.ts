0369cb81790ca23c5b6a476209a615fbb8055db7
koros <korosmatick@gmail.com>
2024-02-27 19:48:18 +0300

    address pr comments

--- packages/http/fetch/test/browser/index.ts
+++ packages/http/fetch/test/browser/index.ts
@@ -2,5 +2,5 @@ export * from "../common/middleware/headersUtil";
 export * from "../common/middleware/retryHandler";
 export * from "../common/middleware/retryHandlerOptions";
 export * from "./httpClient";
-export * from "./MiddlewareFactory";
+export * from "./middlewareFactory";
 export * from "./kiotaClientFactory";
