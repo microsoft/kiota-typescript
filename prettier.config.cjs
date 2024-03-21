e1aacf95a8ed2c5e71eeb5461bf846c937da97ea
Musale Martin <martinmusale@microsoft.com>
2024-03-21 19:46:18 +0300

    Initial removal of cjs on http/fetch

---
+++ prettier.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+	...require('@microsoft/eslint-config-msgraph/prettier.config'),
+	embeddedLanguageFormatting: "off",
+	"arrowParens": "always",
+	"bracketSpacing": true,
+	"jsxBracketSameLine": true,
+	"jsxSingleQuote": false,
+	"printWidth": 5000,
+	"proseWrap": "never",
+	"semi": true,
+	"singleQuote": false,
+	"tabWidth": 4,
+	"trailingComma": "all",
+	"useTabs": true
+}
