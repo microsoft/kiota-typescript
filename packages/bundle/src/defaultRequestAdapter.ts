1729b740ff463c6809c1b5366286ce930767dbfe
Andrew Omondi <anomondi@microsoft.com>
2024-07-30 14:58:54 +0300

    Format

--- packages/bundle/src/defaultRequestAdapter.ts
+++ packages/bundle/src/defaultRequestAdapter.ts
@@ -5,56 +5,37 @@
  * -------------------------------------------------------------------------------------------
  */
 
-import {
-  AuthenticationProvider,
-  ParseNodeFactory,
-  ParseNodeFactoryRegistry,
-  registerDefaultDeserializer,
-  registerDefaultSerializer,
-  SerializationWriterFactory,
-  SerializationWriterFactoryRegistry,
-} from "@microsoft/kiota-abstractions";
-import { FormParseNodeFactory, FormSerializationWriterFactory } from '@microsoft/kiota-serialization-form';
-import { JsonParseNodeFactory, JsonSerializationWriterFactory } from '@microsoft/kiota-serialization-json';
-import { MultipartSerializationWriterFactory } from '@microsoft/kiota-serialization-multipart';
-import { TextParseNodeFactory, TextSerializationWriterFactory } from '@microsoft/kiota-serialization-text';
-import {
-  FetchRequestAdapter,
-  HttpClient,
-  type ObservabilityOptions,
-  ObservabilityOptionsImpl,
-} from "@microsoft/kiota-http-fetchlibrary";
+import { AuthenticationProvider, ParseNodeFactory, ParseNodeFactoryRegistry, registerDefaultDeserializer, registerDefaultSerializer, SerializationWriterFactory, SerializationWriterFactoryRegistry } from "@microsoft/kiota-abstractions";
+import { FormParseNodeFactory, FormSerializationWriterFactory } from "@microsoft/kiota-serialization-form";
+import { JsonParseNodeFactory, JsonSerializationWriterFactory } from "@microsoft/kiota-serialization-json";
+import { MultipartSerializationWriterFactory } from "@microsoft/kiota-serialization-multipart";
+import { TextParseNodeFactory, TextSerializationWriterFactory } from "@microsoft/kiota-serialization-text";
+import { FetchRequestAdapter, HttpClient, type ObservabilityOptions, ObservabilityOptionsImpl } from "@microsoft/kiota-http-fetchlibrary";
 
 /**
  * Default request adapter for graph clients. Bootstraps serialization and other aspects.
  */
 export class DefaultRequestAdapter extends FetchRequestAdapter {
-  /**
-   * Instantiates a new request adapter.
-   * @param authenticationProvider the authentication provider to use.
-   * @param parseNodeFactory the parse node factory to deserialize responses.
-   * @param serializationWriterFactory the serialization writer factory to use to serialize request bodies.
-   * @param httpClient the http client to use to execute requests.
-   * @param observabilityOptions the observability options to use.
-   */
-  public constructor(
-    authenticationProvider: AuthenticationProvider,
-    parseNodeFactory: ParseNodeFactory = ParseNodeFactoryRegistry.defaultInstance,
-    serializationWriterFactory: SerializationWriterFactory = SerializationWriterFactoryRegistry.defaultInstance,
-    httpClient: HttpClient = new HttpClient(),
-    observabilityOptions: ObservabilityOptions = new ObservabilityOptionsImpl(),
-  ) {
-    super(authenticationProvider, parseNodeFactory, serializationWriterFactory, httpClient, observabilityOptions);
-    DefaultRequestAdapter.setupDefaults();
-  }
+	/**
+	 * Instantiates a new request adapter.
+	 * @param authenticationProvider the authentication provider to use.
+	 * @param parseNodeFactory the parse node factory to deserialize responses.
+	 * @param serializationWriterFactory the serialization writer factory to use to serialize request bodies.
+	 * @param httpClient the http client to use to execute requests.
+	 * @param observabilityOptions the observability options to use.
+	 */
+	public constructor(authenticationProvider: AuthenticationProvider, parseNodeFactory: ParseNodeFactory = ParseNodeFactoryRegistry.defaultInstance, serializationWriterFactory: SerializationWriterFactory = SerializationWriterFactoryRegistry.defaultInstance, httpClient: HttpClient = new HttpClient(), observabilityOptions: ObservabilityOptions = new ObservabilityOptionsImpl()) {
+		super(authenticationProvider, parseNodeFactory, serializationWriterFactory, httpClient, observabilityOptions);
+		DefaultRequestAdapter.setupDefaults();
+	}
 
-  private static setupDefaults() {
-    registerDefaultSerializer(JsonSerializationWriterFactory);
-    registerDefaultSerializer(TextSerializationWriterFactory);
-    registerDefaultSerializer(FormSerializationWriterFactory);
-    registerDefaultSerializer(MultipartSerializationWriterFactory);
-    registerDefaultDeserializer(JsonParseNodeFactory);
-    registerDefaultDeserializer(TextParseNodeFactory);
-    registerDefaultDeserializer(FormParseNodeFactory);
-  }
+	private static setupDefaults() {
+		registerDefaultSerializer(JsonSerializationWriterFactory);
+		registerDefaultSerializer(TextSerializationWriterFactory);
+		registerDefaultSerializer(FormSerializationWriterFactory);
+		registerDefaultSerializer(MultipartSerializationWriterFactory);
+		registerDefaultDeserializer(JsonParseNodeFactory);
+		registerDefaultDeserializer(TextParseNodeFactory);
+		registerDefaultDeserializer(FormParseNodeFactory);
+	}
 }
--- packages/bundle/test/bundleTests.ts
+++ packages/bundle/test/bundleTests.ts
@@ -9,39 +9,37 @@ import { assert, describe, it } from "vitest";
 
 import { DefaultRequestAdapter } from "../src/defaultRequestAdapter";
 
-
 describe("defaultRequestAdapter.ts", () => {
-  describe("Initialization", () => {
-    it("should throw error on null authProvider", async () => {
-      assert.throws(() => new DefaultRequestAdapter(null as unknown as AuthenticationProvider), Error, "authentication provider cannot be null");
-    });
+	describe("Initialization", () => {
+		it("should throw error on null authProvider", async () => {
+			assert.throws(() => new DefaultRequestAdapter(null as unknown as AuthenticationProvider), Error, "authentication provider cannot be null");
+		});
 
-    it("should setup serializers correctly", async () => {
-      const requestAdapter = new DefaultRequestAdapter(new AnonymousAuthenticationProvider());
+		it("should setup serializers correctly", async () => {
+			const requestAdapter = new DefaultRequestAdapter(new AnonymousAuthenticationProvider());
 
-      assert.isNotNull(requestAdapter);
+			assert.isNotNull(requestAdapter);
 
-      const serializerMap = SerializationWriterFactoryRegistry.defaultInstance.contentTypeAssociatedFactories;
-      const deserializerMap = ParseNodeFactoryRegistry.defaultInstance.contentTypeAssociatedFactories;
+			const serializerMap = SerializationWriterFactoryRegistry.defaultInstance.contentTypeAssociatedFactories;
+			const deserializerMap = ParseNodeFactoryRegistry.defaultInstance.contentTypeAssociatedFactories;
 
-      assert.isNotNull(serializerMap);
-      assert.isNotNull(deserializerMap);
+			assert.isNotNull(serializerMap);
+			assert.isNotNull(deserializerMap);
 
-      // verify that the default serializers are registered
-      assert.equal(serializerMap.size, 4);
-      assert.equal(deserializerMap.size, 3);
+			// verify that the default serializers are registered
+			assert.equal(serializerMap.size, 4);
+			assert.equal(deserializerMap.size, 3);
 
-      // verify that the default serializers are registered by name
-      assert.isTrue(serializerMap.has("application/json"));
-      assert.isTrue(serializerMap.has("text/plain"));
-      assert.isTrue(serializerMap.has("application/x-www-form-urlencoded"));
-      assert.isTrue(serializerMap.has("multipart/form-data"));
+			// verify that the default serializers are registered by name
+			assert.isTrue(serializerMap.has("application/json"));
+			assert.isTrue(serializerMap.has("text/plain"));
+			assert.isTrue(serializerMap.has("application/x-www-form-urlencoded"));
+			assert.isTrue(serializerMap.has("multipart/form-data"));
 
-      // verify that the default deserializers are registered by name
-      assert.isTrue(deserializerMap.has("application/json"));
-      assert.isTrue(deserializerMap.has("text/plain"));
-      assert.isTrue(deserializerMap.has("application/x-www-form-urlencoded"));
-
-    });
-  });
+			// verify that the default deserializers are registered by name
+			assert.isTrue(deserializerMap.has("application/json"));
+			assert.isTrue(deserializerMap.has("text/plain"));
+			assert.isTrue(deserializerMap.has("application/x-www-form-urlencoded"));
+		});
+	});
 });
