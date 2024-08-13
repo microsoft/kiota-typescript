6a79ae2743375657042a0728e21f855dd65a891e
Andrew Omondi <anomondi@microsoft.com>
2024-08-13 20:37:10 +0300

    Lint

--- packages/test/tests/getTest.ts
+++ packages/test/tests/getTest.ts
@@ -10,31 +10,31 @@ import { proxyClient, userId } from "./testClient";
 import { assert, describe, it } from "vitest";
 
 describe("TestGet", () => {
-  it("should validate empty params in indexer", async () => {
-    assert.throws(() => proxyClient.users.byUserId("").messages.byMessageId("").toGetRequestInformation(), 'path parameter user%2Did cannot be undefined');
-    assert.throws(() => proxyClient.users.byUserId("").messages.byMessageId("").toGetRequestInformation(), 'path parameter user%2Did cannot be undefined');
-    assert.throws(() => proxyClient.users.byUserId("sample").messages.byMessageId("").toGetRequestInformation(), 'path parameter message%2Did cannot be undefined');
-  });
-  it("should return a test", async () => {
-    const messages = await proxyClient.users.byUserId(userId).messages.get();
-    assert.isDefined(messages?.value);
-  });
-  it("should decode query parameters", async () => {
-    const messages = await proxyClient.users.byUserId(userId).messages.get({
-      queryParameters: {
-        select: ["subject"],
-        search: "test",
-        count: true,
-      },
-    });
-    const messagesRaw = proxyClient.users.withUrl("foo");
-    assert.isDefined(messagesRaw);
-    const messagesRI = proxyClient.users.byUserId(userId).messages.toGetRequestInformation({
-      queryParameters: {
-        count: true,
-      },
-    });
-    assert.equal(`https://graph.microsoft.com/v1.0/users/${userId}/messages?%24count=true`, messagesRI.URL);
-    assert.isDefined(messages?.value);
-  });
+	it("should validate empty params in indexer", async () => {
+		assert.throws(() => proxyClient.users.byUserId("").messages.byMessageId("").toGetRequestInformation(), "path parameter user%2Did cannot be undefined");
+		assert.throws(() => proxyClient.users.byUserId("").messages.byMessageId("").toGetRequestInformation(), "path parameter user%2Did cannot be undefined");
+		assert.throws(() => proxyClient.users.byUserId("sample").messages.byMessageId("").toGetRequestInformation(), "path parameter message%2Did cannot be undefined");
+	});
+	it("should return a test", async () => {
+		const messages = await proxyClient.users.byUserId(userId).messages.get();
+		assert.isDefined(messages?.value);
+	});
+	it("should decode query parameters", async () => {
+		const messages = await proxyClient.users.byUserId(userId).messages.get({
+			queryParameters: {
+				select: ["subject"],
+				search: "test",
+				count: true,
+			},
+		});
+		const messagesRaw = proxyClient.users.withUrl("foo");
+		assert.isDefined(messagesRaw);
+		const messagesRI = proxyClient.users.byUserId(userId).messages.toGetRequestInformation({
+			queryParameters: {
+				count: true,
+			},
+		});
+		assert.equal(`https://graph.microsoft.com/v1.0/users/${userId}/messages?%24count=true`, messagesRI.URL);
+		assert.isDefined(messages?.value);
+	});
 });
