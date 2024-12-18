d18bb7f9bd84c06f3f7ed66b6cd5e730125f5f69
Vincent Biret <vibiret@microsoft.com>
2024-12-18 12:28:53 -0500

    chore: removes redundant time test
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- packages/abstractions/test/common/requestInformation.ts
+++ packages/abstractions/test/common/requestInformation.ts
@@ -223,13 +223,13 @@ describe("RequestInformation", () => {
 	});
 
 	it("should correctly handle custom type in query/path parameter", () => {
-		const expected: string = `http://localhost/users/33933a8d-32bb-c6a8-784a-f60b5a1dd66a/2021-12-12?objectId=83afbf49-5583-152c-d7fb-176105d518bc&startDate=2021-12-12&startTime=23%3A12%3A00.0000000&timeStamp=2024-06-11T00%3A00%3A00.000Z&duration=P1D&time=23:12:00.0000000`;
+		const expected: string = `http://localhost/users/33933a8d-32bb-c6a8-784a-f60b5a1dd66a/2021-12-12?objectId=83afbf49-5583-152c-d7fb-176105d518bc&startDate=2021-12-12&startTime=23%3A12%3A00.0000000&timeStamp=2024-06-11T00%3A00%3A00.000Z&duration=P1D`;
 		const requestInformation = new RequestInformation(HttpMethod.GET);
 		requestInformation.pathParameters["baseurl"] = baseUrl;
 		requestInformation.pathParameters["userId"] = parseGuidString("33933a8d-32bb-c6a8-784a-f60b5a1dd66a");
 		requestInformation.pathParameters["date"] = DateOnly.parse("2021-12-12");
 		requestInformation.urlTemplate = "http://localhost/users/{userId}/{date}{?objectId,startDate,startTime,endDate,endTime,timeStamp,duration}";
-		requestInformation.setQueryStringParametersFromRawObject<GetQueryParameters>({ objectId: parseGuidString("83afbf49-5583-152c-d7fb-176105d518bc"), startDate: new DateOnly({ year: 2021, month: 12, day: 12 }), startTime: new TimeOnly({ hours: 23, minutes: 12 }), timeStamp: new Date("2024-06-11T00:00:00.000Z"), duration: Duration.parse("P1D"), time: TimeOnly.parse("23:12:00.0000000") }, getQueryParameterMapper);
+		requestInformation.setQueryStringParametersFromRawObject<GetQueryParameters>({ objectId: parseGuidString("83afbf49-5583-152c-d7fb-176105d518bc"), startDate: new DateOnly({ year: 2021, month: 12, day: 12 }), startTime: new TimeOnly({ hours: 23, minutes: 12 }), timeStamp: new Date("2024-06-11T00:00:00.000Z"), duration: Duration.parse("P1D") }, getQueryParameterMapper);
 		assert.equal(requestInformation.URL, expected);
 	});
 
