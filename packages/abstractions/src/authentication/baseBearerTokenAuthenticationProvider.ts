5c03e3eeee05af8c9210d754b32d8bee4f57538b
Vincent Biret <vibiret@microsoft.com>
2025-01-31 08:32:19 -0500

    fix: removes extraneous getter

--- packages/abstractions/src/authentication/baseBearerTokenAuthenticationProvider.ts
+++ packages/abstractions/src/authentication/baseBearerTokenAuthenticationProvider.ts
@@ -35,8 +35,4 @@ export class BaseBearerTokenAuthenticationProvider implements AuthenticationProv
 			}
 		}
 	};
-
-	public get accessTokenProviderInstance(): AccessTokenProvider {
-		return this.accessTokenProvider;
-	}
 }
