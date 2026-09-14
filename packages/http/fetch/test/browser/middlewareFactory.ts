241cd41b639ad961fd01b53dbc19a446b5df4dc2
RKS <rajesh.sharma@owasp.org>
2026-09-13 20:51:46 -0400

    feat(http): implement body inspection handler
    Closes #1513

--- packages/http/fetch/test/browser/middlewareFactory.ts
+++ packages/http/fetch/test/browser/middlewareFactory.ts
@@ -6,30 +6,32 @@
  */
 
 import { assert, describe, it } from "vitest";
-import { CustomFetchHandler, HeadersInspectionHandler, MiddlewareFactory, ParametersNameDecodingHandler, RedirectHandler, RetryHandler, UrlReplaceHandler, UserAgentHandler, CompressionHandler } from "../../src";
+import { CustomFetchHandler, HeadersInspectionHandler, BodyInspectionHandler, MiddlewareFactory, ParametersNameDecodingHandler, RedirectHandler, RetryHandler, UrlReplaceHandler, UserAgentHandler, CompressionHandler } from "../../src";
 
 describe("browser - MiddlewareFactory", () => {
 	it("Should return the default pipeline", () => {
 		const defaultMiddleWareArray = MiddlewareFactory.getDefaultMiddlewares();
-		assert.equal(defaultMiddleWareArray.length, 7);
+		assert.equal(defaultMiddleWareArray.length, 8);
 		assert.isTrue(defaultMiddleWareArray[0] instanceof RetryHandler);
 		assert.isTrue(defaultMiddleWareArray[1] instanceof RedirectHandler);
 		assert.isTrue(defaultMiddleWareArray[2] instanceof ParametersNameDecodingHandler);
 		assert.isTrue(defaultMiddleWareArray[3] instanceof UserAgentHandler);
 		assert.isTrue(defaultMiddleWareArray[4] instanceof HeadersInspectionHandler);
-		assert.isTrue(defaultMiddleWareArray[5] instanceof UrlReplaceHandler);
-		assert.isTrue(defaultMiddleWareArray[6] instanceof CustomFetchHandler);
+		assert.isTrue(defaultMiddleWareArray[5] instanceof BodyInspectionHandler);
+		assert.isTrue(defaultMiddleWareArray[6] instanceof UrlReplaceHandler);
+		assert.isTrue(defaultMiddleWareArray[7] instanceof CustomFetchHandler);
 	});
 	it("Should return the performance pipeline", () => {
 		const defaultMiddleWareArray = MiddlewareFactory.getPerformanceMiddlewares();
-		assert.equal(defaultMiddleWareArray.length, 8);
+		assert.equal(defaultMiddleWareArray.length, 9);
 		assert.isTrue(defaultMiddleWareArray[0] instanceof RetryHandler);
 		assert.isTrue(defaultMiddleWareArray[1] instanceof RedirectHandler);
 		assert.isTrue(defaultMiddleWareArray[2] instanceof ParametersNameDecodingHandler);
 		assert.isTrue(defaultMiddleWareArray[3] instanceof UserAgentHandler);
 		assert.isTrue(defaultMiddleWareArray[4] instanceof CompressionHandler);
 		assert.isTrue(defaultMiddleWareArray[5] instanceof HeadersInspectionHandler);
-		assert.isTrue(defaultMiddleWareArray[6] instanceof UrlReplaceHandler);
-		assert.isTrue(defaultMiddleWareArray[7] instanceof CustomFetchHandler);
+		assert.isTrue(defaultMiddleWareArray[6] instanceof BodyInspectionHandler);
+		assert.isTrue(defaultMiddleWareArray[7] instanceof UrlReplaceHandler);
+		assert.isTrue(defaultMiddleWareArray[8] instanceof CustomFetchHandler);
 	});
 });
